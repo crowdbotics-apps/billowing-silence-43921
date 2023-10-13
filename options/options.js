e9ad6564a112f7e45077ccc8f0d854d5ef818e72
Crowdbotics <team@crowdbotics.com>
2023-10-13 12:28:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-silence-43921",
+  url: "https://billowing-silence-43921.botics.co",
+  api: "https://billowing-silence-43921.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
